4cd6dc3b04de6efdf16f461bb25a6158c371f34b
Olga Botvinnik <olga.botvinnik@bridgebio.com>
2023-03-02 13:19:20 -0800

    Add insertion counter for Translation Bams

--- bin/translate_bams.py
+++ bin/translate_bams.py
@@ -95,7 +95,7 @@ def translate(dna):
         # if protein[dna_sequence[i:i+3]] == "STOP" :
         #     break
         try:
-            protein_sequence += protein[dna[i : i + 3]]
+            protein_sequence += protein[dna[i: i + 3]]
         except KeyError:
             raise ValueError(
                 "One or more N present in non-degenerate position -- Cannot translate"
@@ -103,49 +103,74 @@ def translate(dna):
     return protein_sequence
 
 
-def count_21nt_7aa(bams, directed_evolution_interval):
-    sequence_counter = {}
-    non_translated_counter = {}
-    cigar_strings = {}
+class InsertionCounter:
+    def __init__(self, bams, directed_evolution_interval, prefix):
 
-    for bam in bams:
-        sample_name = os.path.basename(bam).split(".")[0]
-        sequence_counter[sample_name] = Counter()
-        non_translated_counter[sample_name] = Counter()
-        cigar_strings[sample_name] = Counter()
+        self.bams = bams
+        self.directed_evolution_interval = directed_evolution_interval
+        self.prefix = prefix
+        self.sequence_counter = {}
+        self.non_translated_counter = {}
+        self.cigar_strings = {}
 
-        # create a bam files
-        bamfile = pysam.AlignmentFile(bam, "rb")
+    def count_27nt_insertions(self):
 
-        # For all reads covering the directed evolution interval
-        for i, read in enumerate(bamfile.fetch('amplicon', *directed_evolution_interval)):
-            cigar_strings[sample_name][read.cigarstring] += 1
+        for bam in self.bams:
+            sample_name = os.path.basename(bam).split(".")[0]
+            self.sequence_counter[sample_name] = Counter()
+            self.non_translated_counter[sample_name] = Counter()
+            self.cigar_strings[sample_name] = Counter()
+
+            read_id_to_classification = {}
+
+            # create a bam files
+            bamfile = pysam.AlignmentFile(bam, "rb")
+
+            # For all reads covering the directed evolution interval
+            self.classify_reads_in_interval(bamfile, read_id_to_classification, sample_name)
+
+            for read in bamfile:
+                if read.query_name not in read_id_to_classification:
+                    self.increment_counter_classify_read(read, read_id_to_classification, sample_name,
+                                                         category="doesnt_overlap_nns_region")
+            read_id_to_classification_series = pd.Series(read_id_to_classification, name=f'{sample_name}_per_read')
+            read_id_to_classification_series.to_csv(f'{sample_name}__per_read_categories.csv')
+
+        cigar_strings_df, non_translated_df, sequence_counter_df = self._make_dataframes(sample_name)
+
+        return sequence_counter_df, non_translated_df, cigar_strings_df
+
+    def _make_dataframes(self, sample_name):
+        sequence_counter_df = (
+            pd.DataFrame(self.sequence_counter).fillna(0).astype(int).sort_index(ascending=True)
+        )
+        # Sort according to first item, arbitrarily
+        sequence_counter_df = sequence_counter_df.sort_values(
+            sequence_counter_df.columns[0], ascending=False
+        )
+        sequence_counter_df.index.names = ["aa_7mer", "nt_21mer"]
+        non_translated_df = pd.DataFrame(self.non_translated_counter).fillna(0).astype(int)
+        cigar_strings_df = pd.DataFrame(self.cigar_strings).sort_values(
+            sample_name, ascending=False
+        )
+        return cigar_strings_df, non_translated_df, sequence_counter_df
+
+    def classify_reads_in_interval(self, bamfile, read_id_to_classification, sample_name):
+        for i, read in enumerate(bamfile.fetch('amplicon', *self.directed_evolution_interval)):
+            self.cigar_strings[sample_name][read.cigarstring] += 1
 
             if read.mapping_quality < 40:
-                non_translated_counter[sample_name]["low_quality_mapping"] += 1
+                self.increment_counter_classify_read(read, read_id_to_classification, sample_name,
+                                                     category="low_quality_mapping")
                 continue
 
             try:
                 interval_positions = [
                     read.get_reference_positions().index(x)
-                    for x in directed_evolution_interval
+                    for x in self.directed_evolution_interval
                 ]
             except ValueError:
-                deletions = [
-                    (operation, length)
-                    for (operation, length) in read.cigartuples
-                    if operation == 2
-                ]
-                # print(deletions)
-                if len(deletions) == 1:
-                    if deletions[0][1] == 21:
-                        non_translated_counter[sample_name]["wild_type"] += 1
-                    elif deletions[0][1] < 21:
-                        non_translated_counter[sample_name]["insertion_lt_21"] += 1
-                    else:
-                        non_translated_counter[sample_name]["insertion_gt_21"] += 1
-                else:
-                    non_translated_counter[sample_name]["other"] += 1
+                self._check_deletions_wildtype(read, read_id_to_classification, sample_name)
 
                 # Move on to next read
                 continue
@@ -155,41 +180,52 @@ def count_21nt_7aa(bams, directed_evolution_interval):
                 j = interval_positions[1]
                 evolved_seq = read.query_alignment_sequence[i:j]
                 if len(evolved_seq) != 21:
-                    non_translated_counter[sample_name][
-                        f"insertion_{len(evolved_seq)}nt"
-                    ] += 1
+                    self.increment_counter_classify_read(read, read_id_to_classification, sample_name,
+                                                         category=f"insertion_{len(evolved_seq)}nt")
                     continue
                 # print(evolved_seq)
                 try:
                     evolved_seq_translated = translate(evolved_seq)
                 except ValueError:
-                    non_translated_counter[sample_name][
-                        "21nt_insertion_too_many_ns"
-                    ] += 1
+                    self.increment_counter_classify_read(read, read_id_to_classification, sample_name,
+                                                         category="21nt_insertion_too_many_ns")
                     continue
                 assert len(evolved_seq_translated) == 7
                 pair = (evolved_seq_translated, evolved_seq)
-                sequence_counter[sample_name][pair] += 1
-                non_translated_counter[sample_name]["21nt_insertion"] += 1
+                self.sequence_counter[sample_name][pair] += 1
+                self.increment_counter_classify_read(read, read_id_to_classification, sample_name,
+                                                     category="21nt_insertion")
+            else:
+                self.increment_counter_classify_read(read, read_id_to_classification, sample_name,
+                                                     category="not_all")
         else:
-            non_translated_counter[sample_name]["low_quality_mapping"] += 1
+            self.increment_counter_classify_read(read, read_id_to_classification, sample_name,
+                                                 category="low_quality_mapping")
 
-    sequence_counter_df = (
-        pd.DataFrame(sequence_counter).fillna(0).astype(int).sort_index(ascending=True)
-    )
-    # Sort according to first item, arbitrarily
-    sequence_counter_df = sequence_counter_df.sort_values(
-        sequence_counter_df.columns[0], ascending=False
-    )
-    sequence_counter_df.index.names = ["aa_7mer", "nt_21mer"]
+    def _check_deletions_wildtype(self, read, read_id_to_classification, sample_name):
+        deletions = [
+            (operation, length)
+            for (operation, length) in read.cigartuples
+            if operation == 2
+        ]
+        if len(deletions) == 1:
+            if deletions[0][1] == 21:
+                self.increment_counter_classify_read(read, read_id_to_classification, sample_name,
+                                                     category="wild_type")
+            elif deletions[0][1] < 21:
+                self.increment_counter_classify_read(read, read_id_to_classification, sample_name,
+                                                     category="insertion_lt_21")
+            else:
+                self.increment_counter_classify_read(read, read_id_to_classification, sample_name,
+                                                     category="insertion_gt_21")
 
-    non_translated_df = pd.DataFrame(non_translated_counter).fillna(0).astype(int)
+        else:
+            self.increment_counter_classify_read(read, read_id_to_classification, sample_name,
+                                                 category="insertion_gt_21")
 
-    cigar_strings_df = pd.DataFrame(cigar_strings).sort_values(
-        sample_name, ascending=False
-    )
-
-    return sequence_counter_df, non_translated_df, cigar_strings_df
+    def increment_counter_classify_read(self, read, read_id_to_classification, sample_name, category):
+        self.non_translated_counter[sample_name][category] += 1
+        read_id_to_classification[read.query_name] = category
 
 
 def get_directed_evolution_interval(fasta, evolution_sequence):
@@ -200,8 +236,9 @@ def get_directed_evolution_interval(fasta, evolution_sequence):
             end = start + len(evolution_sequence)
             # Only look at the first sequence
             break
-        logger.warn("Only looking at first sequence in fasta file")
+        logger.warning("Only looking at first sequence in fasta file")
     return start, end
+
 
 def get_top_n_per_column(sequence_counts_df, n=10):
     dfs = []
@@ -250,7 +287,6 @@ def main(
     cigar_strings_csv,
     prefix,
 ):
-
     if prefix:
         evolved_sequence_counts = f"{prefix}__{evolved_sequence_counts}"
         insertion_summary = f"{prefix}__{insertion_summary}"
@@ -260,9 +296,9 @@ def main(
         fasta, directed_evolution_sequence
     )
 
-    sequence_counter_df, non_translated_df, cigar_strings_df = count_21nt_7aa(
-        bams, directed_evolution_interval
-    )
+    insertion_counter = InsertionCounter(bams, directed_evolution_interval, prefix)
+
+    sequence_counter_df, non_translated_df, cigar_strings_df = insertion_counter.count_27nt_insertions()
     sequence_counter_df.to_csv(evolved_sequence_counts)
 
     sequence_counter_df_top_n = get_top_n_per_column(sequence_counter_df)
